3035e538dad70b63ab8ca76295a9f6f208946a4e
kudakwashe2018 <57469308+kudakwashe2018@users.noreply.github.com>
2021-02-05 02:12:03 +0200

    Add project files.

---
+++ SimpleCalculator/CalculatorEngine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace SimpleCalculator
+{
+    public class CalculatorEngine
+    {
+        public double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
+        {
+            double result;
+            switch (argOperation.ToLower())
+            {
+                case "ADD":
+                case "Add":
+                case "add":
+                case "+":
+                    result = argFirstNumber + argSecondNumber;
+                    break; 
+                case "Subtract":
+                case "subtract":
+                case "SUBTRACT":
+                case "-":
+                    result = argFirstNumber - argSecondNumber;
+                    break;
+                case "MULTIPLY":
+                case "Multiply":
+                case "multiply":
+                case "*":
+                    result = argFirstNumber * argSecondNumber;
+                    break;
+                case "DIVISION":
+                case "Division":
+                case "division":
+                case "/":
+                    result = argFirstNumber / argSecondNumber;
+                    break;
+                default:
+                    throw new InvalidOperationException("Specified Operation is nor recognized...");
+            }
+            return result;
+        }
+    }
+}
